86883e511b2f17924555c409a2bbdb49b5c5ff5d
Sydney <68250402+cecelot@users.noreply.github.com>
2024-12-01 00:23:11 -0500

    feat(julia/2024): day 1

--- julia/2024/day01.jl
+++ julia/2024/day01.jl
@@ -1,12 +1,27 @@
 include("../common/utils.jl")
 
-partOne = (input) -> length(split(input, "\n"))
-partTwo = (input) -> length(split(input, "\n"))
+function sides(input)
+    leftS, rightS = foldl(((leftS, rightS), line) -> begin
+            left, right = split(line, "   ")
+            (append!(leftS, parse(Int, left)), append!(rightS, parse(Int, right)))
+        end, split(input, "\n"), init=([], []))
+    return (sort(leftS), sort(rightS))
+end
+
+function partOne(input)
+    leftS, rightS = sides(input)
+    return foldl((total, (left, right)) -> total + abs(left - right), zip(leftS, rightS), init=0)
+end
+
+function partTwo(input)
+    leftS, rightS = sides(input)
+    return foldl((total, left) -> total += left * count(right -> left == right, rightS), leftS, init=0)
+end
 
 common.test(2024, 1,
     partOne,
     partTwo,
     distinct=false,
-    expected=(1, 1),
-    testExpected=(1, 1)
+    expected=(1319616, 27267728),
+    testExpected=(11, 31)
 )
